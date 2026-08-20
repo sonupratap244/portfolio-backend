0f997d2f293cc4e2f1f2d32a83be6d458e80bb0e
Son Pratap <sonpratap.tech@gmail.com>
2026-08-20 23:39:17 +0530

    Fix production CORS origin

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,7 +6,10 @@ import { contactEmailTemplate } from "../utils/emailTemplates.js";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_SECURE === "true",
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
